65ecf4d54647e1349a73cb1c4adbaaa5f6a311dd
SWerllen <583761293@qq.com>
2019-09-09 10:09:00 +0800

    优化标签页

--- 基础37 事件绑定/script.js
+++ 基础37 事件绑定/script.js
@@ -6,11 +6,17 @@ var choose={
 	item3:"frame3",
 };
 
+var l={
+	item1:"0%",
+	item2:"-100%",
+	item3:"-200%",
+};
+
 function refresh(ele){
 	var items=document.querySelectorAll('.item-clicked');
 	for(var i=0;i<items.length;++i){
 		var item=items[i];
-		document.getElementById(choose[item.id]).className="frame-hide";
+		document.getElementById(choose[item.id]).className="frame hide";
 		if(ele===item) {continue;}
 		item.className="item";
 	}
@@ -19,5 +25,6 @@ function refresh(ele){
 function clicked(ele){
 	refresh(ele);
 	ele.className="item-clicked";
-	document.getElementById(choose[ele.id]).className="frame-show";
+	var container=document.getElementById("container");
+	container.style.transform="translate("+l[ele.id]+",0px)";
 }
